8111be313ec8fe2d65e65331abb8a3997518269d
dibinantonyjose <dibinantonyjose@gmail.com>
2025-08-29 22:34:17 +0530

    feat: v2 project setup reactjs19+vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,10 @@
-import React from 'react';
-import './index.css';
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <section>
-      <div>
-        <h1>Dibin Jose</h1>
-        <p>
-          A software engineer (especially the backend) who strives to learn new things every day to
-          become a better version of myself. Currently residing in India, working as a backend
-          developer.
-        </p>
-        <p>
-          Apart from coding, I am mostly into books, films, and travel.
-        </p>
-      </div>
-      <div>
-        <p>dibinantonyjose@gmail.com</p>
-      </div>
-      <div className="contacts">
-        <a href="https://twitter.com/dibinantonyjose" target="_blank" rel="noreferrer">twitter</a>
-        <a href="https://www.linkedin.com/in/dibinantonyjose/" target="_blank" rel="noreferrer">linkedin</a>
-        <a href="https://github.com/dibinantonyjose" target="_blank" rel="noreferrer">github</a>
-        <a href="https://gitlab.com/dibinantonyjose" target="_blank" rel="noreferrer">gitlab</a>
-        <a href="https://stackoverflow.com/users/11405308/dibinantonyjose" target="_blank" rel="noreferrer">stack overflow</a>
-        <a href="https://www.goodreads.com/dibinantonyjose" target="_blank" rel="noreferrer">goodreads</a>
-        <a href="https://www.imdb.com/user/ur144606862/" target="_blank" rel="noreferrer">imdb</a>
-        <a href="https://www.instagram.com/dibinantonyjose/" target="_blank" rel="noreferrer">instagram</a>
-      </div>
-    </section>
+    <>
+      <Header />
+    </>
   );
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+function Header() {
+  return (
+    <Header>
+      <nav>
+        <h1>Dibin Jose</h1>
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </Header>
+  );
+}
+
+export default Header;
