fc5cc120752547d087419c288d1e7b5cb42e18a4
dibinantonyjose <dibinantonyjose@gmail.com>
2025-11-02 10:53:55 +0530

    Refactor: remove unwanted parts

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,11 +1,7 @@
 import {
   faGithub,
-  faGoodreads,
-  faImdb,
-  faInstagram,
   faLinkedinIn,
   faStackOverflow,
-  faTwitter,
 } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Contact.css";
@@ -42,52 +38,12 @@ function Contact() {
         </li>
         <li>
           <a
-            href="https://twitter.com/dibinantonyjose"
-            target="_blank"
-            rel="noreferrer"
-            aria-label="Connect with me on twitter"
-          >
-            <FontAwesomeIcon icon={faTwitter} />
-          </a>
-        </li>
-        <li>
-          <a
             href="https://stackoverflow.com/users/11405308/dibinantonyjose"
             target="_blank"
             rel="noreferrer"
             aria-label="Follow me on stack overflow"
           >
             <FontAwesomeIcon icon={faStackOverflow} />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.goodreads.com/dibinantonyjose"
-            target="_blank"
-            rel="noreferrer"
-            aria-label="Follow me on good reads"
-          >
-            <FontAwesomeIcon icon={faGoodreads} />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.imdb.com/user/ur144606862/"
-            target="_blank"
-            rel="noreferrer"
-            aria-label="Follow me on imdb"
-          >
-            <FontAwesomeIcon icon={faImdb} />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.instagram.com/dibinantonyjose/"
-            target="_blank"
-            rel="noreferrer"
-            aria-label="Connect with me on instagram"
-          >
-            <FontAwesomeIcon icon={faInstagram} />
           </a>
         </li>
       </ul>
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,9 @@
 import Contact from "./Contact/Contact";
 import Description from "./Description";
-import Landing from "./Landing/Landing";
 
 function Home() {
   return (
     <>
-      <Landing />
       <Description />
       <Contact />
     </>
